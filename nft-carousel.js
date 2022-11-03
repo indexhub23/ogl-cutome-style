a0d7b7f8ccf301a1f2616b507889fa08d16649ea
indexhub23 <114402336+indexhub23@users.noreply.github.com>
2022-11-03 08:32:26 -0700

    Update and rename ogl-nft-carousel.js to nft-carousel.js

--- nft-carousel.js
+++ nft-carousel.js
@@ -121,38 +121,38 @@ function setupData2() {
     carouselData2.push([
         {
             id: 1,
-            name: "DieHardBirdie Fight Club #001 - BAM!",
-            description: "BAM! Looking through the sniper scope and taking down the enemies from afar is not easy. With patience and precision, victory comes naturally to gaming champion DieHardBirdie. Once he's on the game mode, everybody should watch out for the Birdie call!"
+            name: "Animated Sky Series #001: BRIGHT BIRDIE SKY",
+            description: 'Senior gaming champion DieHardBirdie stands out from the clear waters, bright blue sky, and white clouds like the birds as he exudes a fierce yet calm demeanor only champions can carry. His moniker "DieHardBirdie" came from his ability to imitate bird sounds by whistling. Amazingly talented is an understatement.'
         },
         {
             id: 2,
-            name: "DieHardBirdie Fight Club #002: POW!",
-            description: "POW! He may look very calm but DieHardBirdie is always ready for battle. From his fearless offense to enemy lines to his defense tactics in CS:GO, the Amazing DieHardBirdie is simply an unstoppable force."
+            name: "Animated Sky Series #002: SPACE ROCKS SKY",
+            description: "Does DieHardBirdie blow people's minds with his energy and passion for community? Yes. Is DieHardBirdie cool? Absolutely. The gamer-artist-senior extraordinaire clearly loves fun moments and the space rocks add the perfect touch to the snapshot."
         },
         {
             id: 3,
-            name: "DieHardBirdie Fight Club #003: KAPOW!",
-            description: "Playing games can be a battle of nerves, whoever gets impatient may lose the round. This is where DieHardBirdie's championship experience shines bright. Patience is the key in the game in offense or defense. Now, watch DieHardBirdie go in for the clutch! KAPOW!"
+            name: "Animated Sky Series #003: SNOWY ROSES SKY",
+            description: "White paper roses fall from the sky like a winter wonderland. DieHardBirdie is no stranger to folding beautiful paper roses and lovely dancing figures made of paper and napkins. The gamer grandpa has been an artist for the past 50 years. It's easy for him to fold a paper rose in under a minute, a magic minute."
         },
         {
             id: 4,
-            name: "DieHardBirdie Fight Club #004: BOOM!",
-            description: "BOOM! BOOM! BOOM! The enemy has arrived and now it's up to DieHardBirdie to defend the site. Hiding from plain sight, he goes for a grenade and gun combo and sucessfully thwarts the enemy team's plan. Another sweet victory for DieHardBirdie!"
+            name: "Animated Sky Series #004: SUNNY DONUTS SKY",
+            description: "DieHardBirdie is a lover of life, spends time with his family, has an eye for the arts, is a champion gamer, and appreciates a sugar-coated sunset moment and a sweet round dessert topped with candy sprinkles. He is an equal opportunity donut lover."
         },
         {
             id: 5,
-            name: "DieHardBirdie Fight Club #005: KABAM!",
-            description: "What would you do if a CS:GO champion is on the enemy team? Do your best! You can bet that DieHardBirdie will always do his best to win. Now, time to plant the bomb and take down the defense. Victory in 5, 4, 3, 2 ... 1. KABAM!"
+            name: "Animated Sky Series #005: ZEBRA NIGHT SKY",
+            description: "DieHardBirdie can pull off a cool, classy look even under the night sky filled with cute zebras. Just like how a dragon symbolizes strength, wisdom, and power and zebra represents freedom, individualism, and determination, DieHardBirdie believes that everyone has a chance to grow, be authentic, and paint stripes of possibility into their life at any age."
         },
         {
             id: 6,
-            name: "DieHardBirdie Fight Club #006: WHAM!",
-            description: "WHAM! Don't let your guard down when playing games like CS:GO or Call of Duty. Enemies can always sneak up on you. Just like DieHardBirdie, move fast to secure the win. Don't forget: Always watch your six!"
+            name: "Animated Sky Series #006: SKY BOOKS EXPLOSION",
+            description: "Reading a good book is like igniting a mix of imagination, adventure, reality, fantasy, and more. DieHardBirdie tip: Relax and pick up a book to keep your mind and imagination active. Or better yet, turn it into a creative masterpiece! DieHardBirdie can turn books into a paper sculpture in a breeze, turning art and literature into one."
         },
         {
             id: 7,
-            name: "DieHardBirdie Fight Club #007: SLAP!",
-            description: "SLAP! CLAP! SNAP! Playing games can help relieve stress and bring you, your family, and friends closer together. Whatever games you may play, playing games help bring people together, not tear people apart. DieHardBirdie hopes for more people to play with their families and friends. Play a game today. GLHF! (Good Luck Have Fun)"
+            name: "Animated Sky Series #007: MIGHTY SWEDISH SKY",
+            description: 'Hej! The world senior CS:GO champion DieHardBirdie hails from Sweden - a kingdom waving the blue and yellow flag and the land with ties to Vikings, Abba, and global conglomerate Ikea. Abbe "DieHardBirdie" Borg has a profound love for his beloved Sweden and he is proud to be a Swede. Ha det bra!'
         },
     ]);
 }
